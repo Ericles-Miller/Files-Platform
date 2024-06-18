b50359546c7f023ee36738f5ecc32cc509be2e19
Éricles Miller <ericlesmiller15@gmail.com>
2024-06-18 16:26:56 -0300

    feat: apply lint code to users controllers

--- src/Api/Controllers/users/UpdateUserController.ts
+++ src/Api/Controllers/users/UpdateUserController.ts
@@ -1,16 +1,19 @@
+import { Request, Response } from 'express';
+
 import { UpdateUserUseCase } from '@Applications/UseCases/users/UpdateUserUseCase';
 import { container } from '@IoC/index';
-import { Request, Response } from 'express';
 
 export class UpdateUserController {
   async handle(request: Request, response: Response) : Promise<Response> {
     const { enable, name, password } = request.body;
-    const file = request.file;
+    const { file } = request;
     const id = request.userId;
 
     const updateUserUseCase = container.get(UpdateUserUseCase);
 
-    await updateUserUseCase.execute({ id, enable, file, name,password });
+    await updateUserUseCase.execute({
+      id, enable, file, name, password,
+    });
 
     return response.status(204).send();
   }
