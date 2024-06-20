0f8e3818b81a9372e944080cfd375dd34c286a47
Ericles-Millere <ericlesmiller15@gmail.com>
2024-06-19 21:31:53 -0300

    fix: ident content email

--- src/Applications/Services/email/SendEmail.ts
+++ src/Applications/Services/email/SendEmail.ts
@@ -3,44 +3,39 @@ import { AppError } from '@Domain/Exceptions/AppError';
 import { Mailer } from '@Jobs/Mailer';
 
 export async function sendEmail(email: string, name: string, token: string) : Promise<void> {
-  const url = `localhost:${process.env.PORT}/sessions/accountsConfirm/${token}`;
+  const url = `${process.env.PATH_CONFIRM_TOKEN}${token}`;
+  console.log(url);
+
   const subject = ' Confirm your account on All Safe Cloud';
   const from = process.env.EMAIL_LOGIN;
   const to = email;
-  const text = `Hii ${name},
-
-    Welcome to All Safe Cloud Platform!
-
-    To complete your account creation and start taking advantage of all our features, you need to confirm your email address.
-
-    How to confirm your email:
-
-    Click on the following link: ${url}
-    Follow the on-screen instructions to complete the scan.
-    Why is it important to confirm your email?
-
-    Confirming your email ensures that you can receive important notifications from the platform, such as file sharing warnings, 
-    link expiration reminders, and security updates.
-    It also helps protect your account from unauthorized access.
-    What can you do on All Safe Cloud?
-
-    Store your files securely in the cloud.
-    Share files with friends, family and co-workers.
-    Access your files from anywhere, at any time.
-    Collaborate on documents with others in real time.
-    Automatically back up your files to prevent data loss.
-    We look forward to you being part of our community!
-
-    Yours sincerely,
-
-    All Safe Cloud Team
+  const html = ` <p>Hi ${name},</p>
+    <p>Welcome to All Safe Cloud Platform!</p>
+    <p>To complete your account creation and start taking advantage of all our features, you need to confirm your email address.</p>
+    <p>How to confirm your email:</p>
+    <p>Click on the following link: <a href="http://localhost:3000/sessions/recover/">Confirm Email</a></p>
+    <p>Follow the on-screen instructions to complete the scan.</p>
+    <p>Why is it important to confirm your email?</p>
+    <p>Confirming your email ensures that you can receive important notifications from the platform, such as file sharing warnings, 
+    link expiration reminders, and security updates. It also helps protect your account from unauthorized access.</p>
+    <p>What can you do on All Safe Cloud?</p>
+    <ul>
+      <li>Store your files securely in the cloud.</li>
+      <li>Share files with friends, family and co-workers.</li>
+      <li>Access your files from anywhere, at any time.</li>
+      <li>Collaborate on documents with others in real time.</li>
+      <li>Automatically back up your files to prevent data loss.</li>
+    </ul>
+    <p>We look forward to you being part of our community!</p>
+    <p>Yours sincerely,</p>
+    <p>All Safe Cloud Team</p>
   `;
 
   if (!from) {
     throw new AppError('The variable from present in .env is null', 404);
   }
 
-  const emailOptions = new EmailOptions(to, subject, text, from);
+  const emailOptions = new EmailOptions(to, subject, html, from);
 
   const mailer = new Mailer();
 
--- src/Domain/Entities/EmailOptions.ts
+++ src/Domain/Entities/EmailOptions.ts
@@ -1,13 +1,13 @@
 export class EmailOptions {
   to: string;
   subject: string;
-  text: string;
+  html: string;
   from: string;
 
-  constructor(to: string, subject: string, text: string, from: string) {
+  constructor(to: string, subject: string, html: string, from: string) {
     this.from = from;
     this.to = to;
     this.subject = subject;
-    this.text = text;
+    this.html = html;
   }
 }
