e7ab71c182120b1de98cf6834a16ee8544a7e5d3
Éricles Miller <ericlesmiller15@gmail.com>
2024-07-01 17:39:01 -0300

    feat: implement documentation to update status sharedItems

--- src/Api/Controllers/sharedItems/UpdateViewSharedItemsController.ts
+++ src/Api/Controllers/sharedItems/UpdateViewSharedItemsController.ts
@@ -7,11 +7,11 @@ import { container } from '@IoC/index';
 export class UpdateViewSharedItemsController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { userId } = request;
-    const { id, option } = request.body;
+    const { id, status } = request.body;
 
     const updateViewSharedItemsUseCase = container.get(UpdateViewSharedItemsUseCase);
 
-    await updateViewSharedItemsUseCase.execute(id, userId, option);
+    await updateViewSharedItemsUseCase.execute(id, userId, status);
 
     return response.status(204).send();
   }
