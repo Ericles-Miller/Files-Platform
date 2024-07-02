282bd519aa69b1520d90290b4cab6e5aec0c1eb7
Éricles Miller <ericlesmiller15@gmail.com>
2024-07-02 11:47:45 -0300

    feat: create documentation to method shared with items to user

--- src/Applications/UseCases/sharedItems/SharedItemsToUserUseCase.ts
+++ src/Applications/UseCases/sharedItems/SharedItemsToUserUseCase.ts
@@ -20,25 +20,33 @@ export class SharedItemsToUserUseCase {
   ) {}
 
   async execute(sharedWithUserId: string): Promise<ISharedWithUserDTO> {
-    const responseItems: ISharedWithUserDTO = { files: [], folders: [] };
+    try {
+      const responseItems: ISharedWithUserDTO = { files: [], folders: [] };
 
-    const sharedItems = await this.sharedItemsRepository.findBySharedUserId(sharedWithUserId);
-    if (!sharedItems) {
-      throw new AppError('The sharedWithUserId does not exists', 404);
-    }
-
-    await Promise.all(sharedItems.map(async (item) => {
-      if (item.sharedStatus) {
-        if (item.folderId) {
-          const folder = await this.foldersRepository.findFoldersShared(item.folderId, sharedWithUserId);
-          if (folder) {
-            responseItems.folders.push(folder);
-          }
-          responseItems.files = await this.findFilesChildrenUseCase.execute({ id: item.folderId, userId: item.userId });
-        }
+      const sharedItems = await this.sharedItemsRepository.findBySharedUserId(sharedWithUserId);
+      if (!sharedItems) {
+        throw new AppError('The sharedWithUserId does not exists!', 404);
       }
-    }));
 
-    return responseItems;
+      await Promise.all(sharedItems.map(async (item) => {
+        if (item.sharedStatus) {
+          if (item.folderId) {
+            const folder = await this.foldersRepository.findFoldersShared(item.folderId, item.userId);
+            if (folder) {
+              responseItems.folders.push(folder);
+            }
+            responseItems.files = await this.findFilesChildrenUseCase.execute({ id: item.folderId, userId: item.userId });
+          }
+        }
+      }));
+
+      return responseItems;
+    } catch (error) {
+      if (error instanceof AppError) {
+        throw error;
+      }
+      console.log(error);
+      throw new AppError('Unexpected server error!', 500);
+    }
   }
 }
